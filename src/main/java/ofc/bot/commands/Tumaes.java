f404da21c1a66403eba3c2f9b666808de5cc7d2d
kiLeo13 <leonardo.silvavieira@hotmail.com>
2024-05-12 02:31:14 -0300

    Update Tumaes.java
    * Fix exception where the file was not being found by the bot due to previous updates that changed some paths.

--- src/main/java/ofc/bot/commands/Tumaes.java
+++ src/main/java/ofc/bot/commands/Tumaes.java
@@ -7,6 +7,7 @@ import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
+import ofc.bot.internal.data.BotFiles;
 import ofc.bot.util.content.annotations.commands.DiscordCommand;
 import ofc.bot.util.content.annotations.commands.CommandPermission;
 import ofc.bot.handlers.commands.contexts.CommandContext;
@@ -24,7 +25,7 @@ import java.util.Random;
 @DiscordCommand(name = "tumaes", description = "GIFs do toquinho.")
 @CommandPermission(Permission.MESSAGE_MANAGE)
 public class Tumaes extends SlashCommand {
-    private static final File file = new File("resources", "tumaes.json");
+    private static final File file = new File(BotFiles.DIR_CONTENT, "tumaes.json");
     private static final Random random = new Random();
     private static final Gson gson = new Gson();
 
