81122fbe6fd6aac3df578256bc6f62a7340e157e
kiLeo13 <leonardo.silvavieira@hotmail.com>
2024-06-01 04:03:42 -0300

    Updated members' roles backup system.
    * Now, the Roles Backup system will also include normal members in the database (from level 40+ or Nitro Boosters).
    * Only privileged members will have their roles backed-up forever, normal members have an expiration of 90 days.

---
+++ src/main/java/ofc/bot/jobs/roles/ExpiredBackupsRemover.java
@@ -0,0 +1,56 @@
+package ofc.bot.jobs.roles;
+
+import ofc.bot.databases.DBManager;
+import ofc.bot.util.Bot;
+import ofc.bot.util.content.annotations.jobs.CronJob;
+import org.jooq.DSLContext;
+import org.jooq.exception.DataAccessException;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
+
+import static ofc.bot.databases.entities.tables.FormerMembersRoles.FORMER_MEMBERS_ROLES;
+
+/**
+ * This class is responsible for removing backups older than 90 days
+ * from the database (table {@link ofc.bot.databases.entities.tables.FormerMembersRoles FormerMembersRoles}) based on the
+ * {@link ofc.bot.databases.entities.tables.FormerMembersRoles#CREATED_AT created_at} column.
+ */
+@CronJob(expression = "0 0 0 ? * * *") // Every day at midnight
+public class ExpiredBackupsRemover implements Job {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExpiredBackupsRemover.class);
+    private static final long EXPIRY_DAYS = TimeUnit.DAYS.toSeconds(90);
+
+    @Override
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+
+        LOGGER.info("Removing expired backups (older than {} days)...", EXPIRY_DAYS);
+
+        try {
+            int deleted = deleteExpiredBackups();
+
+            if (deleted == 0)
+                LOGGER.info("No backups were deleted");
+            else
+                LOGGER.info("Successfully deleted a total of {} backed-up roles", deleted);
+        } catch (DataAccessException e) {
+            LOGGER.error("Could not delete expired backups", e);
+        }
+    }
+
+    private int deleteExpiredBackups() {
+
+        DSLContext ctx = DBManager.getContext();
+        long now = Bot.unixNow();
+        long maxAge = now - EXPIRY_DAYS;
+
+        return ctx.deleteFrom(FORMER_MEMBERS_ROLES)
+                .where(FORMER_MEMBERS_ROLES.CREATED_AT.lessThan(maxAge))
+                .and(FORMER_MEMBERS_ROLES.PRIVILEGED.eq(0))
+                .execute();
+    }
+}
--- src/main/java/ofc/bot/listeners/guilds/roles/MemberRolesBackup.java
+++ src/main/java/ofc/bot/listeners/guilds/roles/MemberRolesBackup.java
@@ -1,11 +1,11 @@
 package ofc.bot.listeners.guilds.roles;
 
-import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberRemoveEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import ofc.bot.util.content.Levels;
 import ofc.bot.util.content.annotations.listeners.EventHandler;
 import ofc.bot.databases.entities.records.FormerMemberRoleRecord;
 import ofc.bot.databases.services.BatchInsertService;
@@ -22,19 +22,22 @@ public class MemberRolesBackup extends ListenerAdapter {
     private static final Logger LOGGER = LoggerFactory.getLogger(MemberRolesBackup.class);
     private static final long TARGET_GUILD = 582430782577049600L;
 
+    private static final List<Levels> ACCEPTED_LEVELS = Levels.fromAbove(Levels.SABITO);
+
     @Override
     public void onGuildMemberRemove(GuildMemberRemoveEvent event) {
 
         Member member = event.getMember();
+        Guild guild = event.getGuild();
 
-        if (member == null)
+        if (member == null || guild.getIdLong() != TARGET_GUILD)
             return;
 
-        Guild guild = event.getGuild();
         Member self = guild.getSelfMember();
-        boolean shouldPersist = shouldPersist(member);
+        boolean privileged = isPrivileged(member);
+        boolean shouldPersist = isMemberIncluded(member) || privileged;
 
-        if (!shouldPersist || guild.getIdLong() != TARGET_GUILD)
+        if (!shouldPersist)
             return;
 
         List<Role> savedRoles = member.getRoles()
@@ -42,10 +45,10 @@ public class MemberRolesBackup extends ListenerAdapter {
                 .filter(self::canInteract)
                 .toList();
 
-        persist(member, savedRoles);
+        saveRoles(member, savedRoles, privileged);
     }
 
-    private void persist(Member member, List<Role> roles) {
+    private void saveRoles(Member member, List<Role> roles, boolean privileged) {
 
         BatchInsertService service = new BatchInsertService();
         String username = member.getUser().getName();
@@ -53,10 +56,9 @@ public class MemberRolesBackup extends ListenerAdapter {
         long guildId = member.getGuild().getIdLong();
 
         for (Role r : roles) {
+            FormerMemberRoleRecord rec = new FormerMemberRoleRecord(userId, guildId, r.getIdLong(), privileged);
 
-            FormerMemberRoleRecord rec = new FormerMemberRoleRecord(userId, guildId, r.getIdLong());
-
-            service.add(rec.getSave());
+            service.add(rec.getSave(false));
         }
 
         try {
@@ -68,20 +70,36 @@ public class MemberRolesBackup extends ListenerAdapter {
         }
     }
 
-    private boolean shouldPersist(Member member) {
+    /*
+     * Privileged members are members who will have their roles persisted forever,
+     * the ExpiredBackupsRemover#EXPIRY_DAYS will not affect their backed-up roles.
+     */
+    private boolean isPrivileged(Member member) {
 
         Guild guild = member.getGuild();
         List<Role> roles = member.getRoles();
         Role salada = Roles.SALADA.toRole(guild);
-        Role staff = Staff.GENERAL.toRole(guild);
-        boolean isManageServer = member.hasPermission(Permission.MANAGE_SERVER);
 
-        if (isManageServer)
+        if (Staff.isStaff(member))
             return true;
 
-        if (salada != null && roles.contains(salada))
-            return true;
+        return salada != null && roles.contains(salada);
+    }
 
-        return staff != null && roles.contains(staff);
+    /*
+     * This method checks whether the given Member should have its roles backed up
+     * in the database according to their MEE6 level, or if Member#isBoosting() returns true.
+     */
+    private boolean isMemberIncluded(Member member) {
+        return member.isBoosting() || hasExpectedLevel(member);
+    }
+
+    private boolean hasExpectedLevel(Member member) {
+
+        Guild guild = member.getGuild();
+        List<Role> roles = member.getRoles();
+
+        return ACCEPTED_LEVELS.stream()
+                .anyMatch(l -> roles.contains(l.role(guild)));
     }
 }
