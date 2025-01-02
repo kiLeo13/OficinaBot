ae2880792ff8785ea02325b603221a97ea9cfa2d
kiLeo13 <leonardo.silvavieira@hotmail.com>
2025-01-01 23:11:39 -0300

    Adjusted StoreItems' names.

--- src/main/java/ofc/bot/domain/entity/enums/StoreItemType.java
+++ src/main/java/ofc/bot/domain/entity/enums/StoreItemType.java
@@ -3,12 +3,12 @@ package ofc.bot.domain.entity.enums;
 import java.util.List;
 
 public enum StoreItemType {
-    GROUP(              "Grupo \uD83D\uDD10",                  800_000),
-    GROUP_TEXT_CHANNEL( "Chat de Texto \uD83D\uDCD3",          550_000),
-    ADDITIONAL_BOT(     "Bot Adicional \uD83E\uDD16",          450_000),
-    GROUP_VOICE_CHANNEL("Chat de Voz \uD83D\uDD0A",            400_000),
-    UPDATE_GROUP(       "Alterar Dados de Grupo \uD83D\uDD8A", 250_000),
-    GROUP_SLOT(         "Vaga de Grupo \uD83D\uDD11",          100_000);
+    GROUP(              "Grupo",           800_000),
+    GROUP_TEXT_CHANNEL( "Chat de Texto",   550_000),
+    ADDITIONAL_BOT(     "Bot Adicional",   450_000),
+    GROUP_VOICE_CHANNEL("Chat de Voz",     400_000),
+    UPDATE_GROUP(       "Modificar Grupo", 250_000),
+    GROUP_SLOT(         "Vaga de Grupo",   100_000);
     
     private final String name;
     private final int price;
