2d566f9fcc5d655daab7bd87d872534c52b29239
kiLeo13 <leonardo.silvavieira@hotmail.com>
2025-03-04 18:23:41 -0300

    Remove problematic bulkSave() methods from Repository class.

--- src/main/java/ofc/bot/domain/sqlite/repository/Repository.java
+++ src/main/java/ofc/bot/domain/sqlite/repository/Repository.java
@@ -63,45 +63,11 @@ public abstract class Repository<T extends OficinaRecord<T>> {
         return ctx.fetch(getTable());
     }
 
-    /**
-     * Bulk inserts all records provided in the {@link List List&lt;T&gt;&nbsp;}{@code recs}.
-     * If any constraints collide, the insertion of that single row is ignored.
-     * <p>
-     * For lists longer than {@value #MAX_BATCH_SIZE} records, the queries will be split
-     * and executed as a {@link Batch} insert.
-     * <p>
-     * The {@code split} argument is ignored (and set to {@code true}),
-     * if the list exceeds the {@link #MAX_BATCH_SIZE} limit.
-     *
-     * @param recs the records to be saved to the database.
-     * @param split whether we should split the queries into a {@link Batch} insert.
-     * @return the amount of rows updated.
-     */
-    public final int bulkSave(@NotNull List<T> recs, boolean split) {
+    public final int bulkSave(@NotNull List<T> recs) {
         Checks.notNull(recs, "Records");
         if (recs.isEmpty()) return 0; // Okay :shrug:
 
-        int size = recs.size();
-        if (size > MAX_BATCH_SIZE || split) {
-            return batch(recs);
-        } else {
-            return saveMultiRow(recs);
-        }
-    }
-
-    public final int bulkSave(@NotNull List<T> recs) {
-        return bulkSave(recs, false);
-    }
-
-    private int saveMultiRow(List<T> recs) {
-        Table<T> table = recs.getFirst().getTable();
-
-        var q = ctx.insertInto(table)
-                .values(recs)
-                .onConflictDoNothing();
-
-        System.out.println(q.getSQL());
-        return 0;
+        return batch(recs);
     }
 
     private int batch(List<T> recs) {
